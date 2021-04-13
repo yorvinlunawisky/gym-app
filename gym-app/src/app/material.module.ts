45b3fc31df8763abf732b62df0bf90a4df495e45
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-12 22:49:48 -0400

    Date picker and checkbox added.

--- gym-app/src/app/material.module.ts
+++ gym-app/src/app/material.module.ts
@@ -3,11 +3,28 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+ 
 
 @NgModule({
-    imports: [MatButtonModule,MatIconModule, MatFormFieldModule, MatInputModule],
-    exports: [MatButtonModule,MatIconModule, MatFormFieldModule, MatInputModule]
+    imports: [
+        MatButtonModule,
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule],
+    exports: [
+        MatButtonModule,
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatCheckboxModule]
 })
 export class MaterialModule {
 

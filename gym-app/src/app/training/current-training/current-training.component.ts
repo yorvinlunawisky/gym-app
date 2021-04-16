e35063da17f5416a910a9043f8d7c9b9bd755e98
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-15 23:42:48 -0400

    Yes and No booleans added to the dialog.

--- gym-app/src/app/training/current-training/current-training.component.ts
+++ gym-app/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 
 import {StopTrainingComponent} from './stop-training.component';
@@ -9,18 +9,23 @@ import {StopTrainingComponent} from './stop-training.component';
   styleUrls: ['./current-training.component.css']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
+    this.startOrResumeTimer();
+  }
+
+  startOrResumeTimer() {
     this.timer = setInterval(() => {
       this.progress = this.progress + 5;
       if(this.progress >= 100) {
         clearInterval(this.timer);
       }
-    }, 1000);  
+    }, 1000);
   }
 
   onStop() {
@@ -29,10 +34,14 @@ export class CurrentTrainingComponent implements OnInit {
       data: {
         progress: this.progress
       }
-    });
+    });    
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
     })
   }
 

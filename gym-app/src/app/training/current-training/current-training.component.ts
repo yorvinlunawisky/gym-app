b14d34a628e9fef25e97550659af835183cdfe8d
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-15 23:05:09 -0400

    A dialog was added.

--- gym-app/src/app/training/current-training/current-training.component.ts
+++ gym-app/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+
+import {StopTrainingComponent} from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -9,7 +12,7 @@ export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: number;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.timer = setInterval(() => {
@@ -22,6 +25,7 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer);
+    this.dialog.open(StopTrainingComponent);
   }
 
 }

c8dbc9355ec2aded22bb936510c0bd37436c1b23
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-23 15:51:56 -0400

    Adding a form to the training component.

--- gym-app/src/app/training/new-training/new-training.component.ts
+++ gym-app/src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
@@ -12,13 +13,13 @@ export class NewTrainingComponent implements OnInit {
   exercises: Exercise[] = [];
 
   constructor(private trainingService: TrainingService) { }
-
+ 
   ngOnInit() {
     this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining() {
-    this.trainingService.startExercise();
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 

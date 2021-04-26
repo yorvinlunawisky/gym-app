a865db137d14598eafd63ecf9d91c1e49bcbeaad
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-04-26 18:08:16 -0400

    Sort and filter functionality added to the past exercises.

--- gym-app/src/app/training/past-training/past-training.component.ts
+++ gym-app/src/app/training/past-training/past-training.component.ts
@@ -1,21 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { MatSort } from '@angular/material/sort'
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
 
+  @ViewChild(MatSort) sort: MatSort;
   constructor(private TrainingService: TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.TrainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 
 }
